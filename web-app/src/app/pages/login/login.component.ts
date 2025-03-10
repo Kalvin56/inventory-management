dfaa68bcfd0a6029e43ba83c1277b2c724a0e6e1
kalvin <kalvin.daniel56@laposte.net>
2025-03-10 10:41:31 +0100

    integrate snackbar system for error handling

--- web-app/src/app/pages/login/login.component.ts
+++ web-app/src/app/pages/login/login.component.ts
@@ -1,25 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { login } from 'src/app/store/actions/auth.action';
 import { isLoggedIn } from 'src/app/store/selectors/auth.selector';
+import { Actions, ofType } from '@ngrx/effects';
+import { tap } from 'rxjs/operators';
+import * as AuthActions from 'src/app/store/actions/auth.action';
+import { SnackbarService } from 'src/app/services/snackbar.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
   loginForm: FormGroup;
   isLoggedIn$: Observable<boolean>;
+  private subscriptions: Subscription[] = [];
 
   constructor(
     private fb: FormBuilder,
     private store: Store<AppState>,
-    private router: Router
+    private router: Router,
+    private snackbarService: SnackbarService,
+    private actions$: Actions
   ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
@@ -29,16 +36,33 @@ export class LoginComponent {
     this.isLoggedIn$ = this.store.select(isLoggedIn);
   }
 
+  ngOnInit() {
+    const loginFailureSubscription = this.actions$.pipe(
+      ofType(AuthActions.loginFailure),
+      tap(({ error }) => {
+        this.snackbarService.showMessage(error.error?.message ?? 'Une erreur est survenue', 'error');
+      })
+    ).subscribe();
+
+    this.subscriptions.push(loginFailureSubscription);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
+
   onSubmit() {
     if (this.loginForm.valid) {
       const credentials = this.loginForm.value;
       this.store.dispatch(login({ credentials }));
 
-      this.isLoggedIn$.subscribe(isLoggedIn => {
+      const isLoggedInSubscription = this.isLoggedIn$.subscribe(isLoggedIn => {
         if (isLoggedIn) {
           this.router.navigate(['/products']);
         }
       });
+
+      this.subscriptions.push(isLoggedInSubscription);
     }
   }
 }
--- web-app/src/app/pages/register/register.component.ts
+++ web-app/src/app/pages/register/register.component.ts
@@ -1,25 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store';
 import { register } from 'src/app/store/actions/auth.action';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription, tap } from 'rxjs';
 import { isLoggedIn } from 'src/app/store/selectors/auth.selector';
+import * as AuthActions from 'src/app/store/actions/auth.action';
+import { Actions, ofType } from '@ngrx/effects';
+import { SnackbarService } from 'src/app/services/snackbar.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   isLoggedIn$: Observable<boolean>;
+  private subscriptions: Subscription[] = [];
 
   constructor(
     private fb: FormBuilder,
     private store: Store<AppState>,
-    private router: Router
+    private router: Router,
+    private snackbarService: SnackbarService,
+    private actions$: Actions
   ) {
     this.registerForm = this.fb.group({
       name: ['', [Validators.required, Validators.maxLength(8)]],
@@ -30,16 +36,34 @@ export class RegisterComponent {
     this.isLoggedIn$ = this.store.select(isLoggedIn);
   }
 
+  ngOnInit() {
+    const registerFailureSubscription =  this.actions$.pipe(
+      ofType(AuthActions.registerFailure),
+      tap(({ error }) => {
+        console.log(error)
+        this.snackbarService.showMessage(error.error?.message ?? 'Une erreur est survenue', 'error');
+      })
+    ).subscribe();
+
+    this.subscriptions.push(registerFailureSubscription);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
+
   onSubmit() {
     if (this.registerForm.valid) {
       const credentials = this.registerForm.value;
       this.store.dispatch(register({ credentials }));
       
-       this.isLoggedIn$.subscribe(isLoggedIn => {
+      const isLoggedInSubscription = this.isLoggedIn$.subscribe(isLoggedIn => {
         if (isLoggedIn) {
           this.router.navigate(['/products']);
         }
       });
+
+      this.subscriptions.push(isLoggedInSubscription);
     }
   }
 }
